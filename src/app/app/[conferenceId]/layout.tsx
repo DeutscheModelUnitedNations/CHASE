fb6672c37fa44a4d30efc59b0edd3ac297944c52
m1212e <14091540+m1212e@users.noreply.github.com>
2025-03-03 00:05:32 +0100

    🐞 fix

--- src/app/app/[conferenceId]/layout.tsx
+++ src/app/app/[conferenceId]/layout.tsx
@@ -1,17 +1,19 @@
 "use client";
 
 import { ConferenceIdContext } from "@/lib/contexts/committee_data";
+import { useParams } from "next/navigation";
 
 export default function MyDelegationLayout({
   children,
-  params,
 }: {
   children: React.ReactNode;
-  params: { conferenceId: string };
 }) {
-  return (
-    <ConferenceIdContext.Provider value={params.conferenceId}>
+  const { conferenceId } = useParams();
+  return conferenceId ? (
+    <ConferenceIdContext.Provider value={conferenceId as string}>
       {children}
     </ConferenceIdContext.Provider>
+  ) : (
+    <div>Loading...</div>
   );
 }
