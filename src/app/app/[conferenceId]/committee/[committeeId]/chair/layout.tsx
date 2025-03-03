4eb5742860079dd9eb9ab63ed4bf597a5081c4d1
m1212e <14091540+m1212e@users.noreply.github.com>
2025-03-04 00:01:36 +0100

    🐞 fix

--- src/app/app/[conferenceId]/committee/[committeeId]/chair/layout.tsx
+++ src/app/app/[conferenceId]/committee/[committeeId]/chair/layout.tsx
@@ -17,15 +17,15 @@ export default function Chair_Pages_Layout({
 }) {
   return (
     <>
-      {/* <SpeakersListMiniatureProvider> */}
-      {/* <SpeakersListMiniature /> */}
-      <MessageCountProvider>
-        <div className="flex h-screen w-screen overflow-hidden bg-white text-primary-100 shadow-md dark:bg-primary-100 dark:text-primary-900">
-          <ChairNavbar />
-          {children}
-        </div>
-      </MessageCountProvider>
-      {/* </SpeakersListMiniatureProvider> */}
+      <SpeakersListMiniatureProvider>
+        <SpeakersListMiniature />
+        {/* <MessageCountProvider> */}
+          <div className="flex h-screen w-screen overflow-hidden bg-white text-primary-100 shadow-md dark:bg-primary-100 dark:text-primary-900">
+            <ChairNavbar />
+            {children}
+          </div>
+        {/* </MessageCountProvider> */}
+      </SpeakersListMiniatureProvider>
     </>
   );
 }
