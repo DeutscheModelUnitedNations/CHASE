22139765e2187d1a2c41035f5a7b03a1db9958c9
m1212e <14091540+m1212e@users.noreply.github.com>
2025-03-03 19:45:14 +0100

    🐞 fix

--- src/app/app/layout.tsx
+++ src/app/app/layout.tsx
@@ -1,11 +1,10 @@
 "use client";
 
-import { useClientSideBackendCall } from "@/lib/backend/useClientSideBackendCall";
 import { BackendTime } from "@/lib/contexts/backendTime";
 import { UserIdentProvider } from "@/lib/contexts/user_ident";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  useClientSideBackendCall((backend) => backend.auth["upsert-self"].post());
+  // useClientSideBackendCall((backend) => backend.auth["upsert-self"].post());
   return (
     <BackendTime>
       <UserIdentProvider>{children}</UserIdentProvider>
